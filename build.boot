cff04254e0e7f38b1099c4b57b7225c71a2a9110
hswick <hswick@smu.edu>
2017-08-06 19:44:44 -0500

    Added Tutorial to README

--- build.boot
+++ build.boot
@@ -1,17 +1,35 @@
 (set-env!
-  :resource-paths #{"src"}
-  :dependencies '[[org.clojure/clojure "1.9.0-alpha15"]
-                  [nightlight "1.6.5" :scope "test"]])
+  :resource-paths #{"src" "data"}
+  :dependencies '[[org.clojure/clojure "1.8.0"]
+                  [org.nd4j/nd4j-native-platform "0.8.0"]
+                  [hswick/jutsu "0.0.4"]
+                  [nightlight "1.7.1" :scope "test"]
+                  [hswick/jutsu.nlp "0.0.2"]
+                  [hswick/jutsu.matrix "0.0.4"]
+                  [cheshire "5.7.1"]])
+
+(task-options!
+       pom {:project 'news-articles-viz
+            :version "0.1.0"}
+       jar {:main 'news-articles.core
+            :manifest {"Description" "totally awesomeness"}})
 
 (require
-  '[nightlight.boot :refer [nightlight]]
-  'jutsu-doc2vec-example.core)
+  '[nightlight.boot :refer [nightlight]])
 
 (deftask night []
   (comp
     (wait)
     (nightlight :port 4000)))
 
+(deftask build []
+  (comp
+    (pom)
+    (jar)
+    (target)))
+
+(require 'news-articles.core)
+
 (deftask run []
   (with-pass-thru _
-    (jutsu-doc2vec-example.core/-main)))
+    (news-articles.core/-main)))
--- src/jutsu_doc2vec_example/core.clj
+++ src/jutsu_doc2vec_example/core.clj
@@ -1,4 +1,80 @@
-(ns jutsu-doc2vec-example.core)
+(ns news-articles.core
+  (:require [jutsu.nlp.core :as nlp]
+            [jutsu.nlp.sentence-iterator :as iter]
+            [jutsu.nlp.util :as util]
+            [jutsu.nlp.tokenization :as token]
+            [jutsu.matrix.core :as m]
+            [cheshire.core :as json]))
+
+;;Only need to run this once to create your word-2-vec model
+;;Then the code can read from the saved model
+(defn train-and-save-w2v []
+  (let [w2v (nlp/word-2-vec
+              (iter/dir-iterator "articles")
+              (token/default-tokenizer-factory)
+              {:min-word-frequency 6
+               :window-size 10
+               :layer-size 150
+               :stopwords (nlp/stop-words)})]
+    (nlp/fit! w2v)
+    (nlp/write-word-vectors w2v "word_vectors.csv")))
+
+;;Turn the files into vectors
+(defn files-2-vecs [w2v dir-path]
+  (let [fs (rest (file-seq (clojure.java.io/file dir-path)))]
+    (map (fn [f] (nlp/doc-2-vec w2v (slurp f))) fs)))
+
+;;Split up vectors by category
+(defn doc-vec-map [w2v base-path categories]
+  (into {}
+    (map (fn [category]
+           [category (files-2-vecs w2v (str base-path category))])
+      categories)))
+
+;;Put document vectors into one big array and run Principal Component Analysis on it
+;;to compress the vectors to 2 dimensions.
+(defn compress-vecs [vecs]
+  (->> (map #(apply m/vstack %) (vals vecs))
+       (apply m/vstack)
+       (m/normalize-zero!)
+       (m/pca 2)))
+
+;;Split vectors back up into 5 categories
+(defn compressed-categories [compressed-vecs file-counts categories]
+  (loop [categories categories i 0 begin 0 new-map {}]
+    (if (empty? categories) new-map
+      (recur 
+        (rest categories) 
+        (inc i) 
+        (+ begin (nth file-counts i))
+        (assoc new-map 
+          (first categories) 
+          (for [n (range begin (+ begin (nth file-counts i)))]
+            (m/get-row compressed-vecs n)))))))
+
+;;Generate data in format to be graphed by jutsu
+(defn gen-graph-data [category-vecs-map]
+  (let [default-map {:mode "markers"
+                     :type "scatter"}]
+    (seq (map
+           (fn [[category vecs]]
+             (merge default-map
+               {:x (map #(m/get-double % 0) vecs)
+                :y (map #(m/get-double % 1) vecs)
+                :name category}))
+           category-vecs-map))))
+
+(defn write-graph-data [graph-data filename]
+  (spit filename (json/generate-string graph-data)))
 
 (defn -main []
-	(println "Hello World"))
+  ;(train-and-save w2v)
+  (let [w2v (nlp/read-word-vectors (clojure.java.io/file "word_vectors.csv"))
+        base-path "data/articles/bbc/"
+        categories ["business" "entertainment" "politics" "sport" "tech"]
+        news-articles-vecs (doc-vec-map w2v base-path categories)
+        file-counts (map count (vals news-articles-vecs))]
+    (-> (compress-vecs news-articles-vecs)
+        (compressed-categories file-counts categories)
+        (gen-graph-data)
+        (write-graph-data "news-articles-graph-data.json"))))
